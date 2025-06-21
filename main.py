29dc4c88a01fc1095b8064c174d950484d241062
bobby838 <838597123scar@gmail.com>
2025-06-21 20:21:55 +0100

    added new features like shooting, asteroids etc.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -8,13 +13,41 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (bullets, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable, drawable)
+    asteroids_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
         screen.fill("black")
+        for object in drawable:
+            object.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.collided(player):
+                print("Game over!")
+                sys.exit()
+            for bullet in bullets:
+                if asteroid.collided(bullet):
+                    bullet.kill()
+                    asteroid.split()
+                    break
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
